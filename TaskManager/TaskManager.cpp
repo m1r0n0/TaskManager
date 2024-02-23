df7fe675e6370b23cb58b6d82ffb4f30cf1b4e20
Oleksandr Myroniuk <Oleksandr.Myroniuk@udelphi.com>
2024-02-23 10:40:27 +0200

    Before UI

--- TaskManager/TaskManager.cpp
+++ TaskManager/TaskManager.cpp
@@ -25,89 +25,6 @@ void printError(TCHAR* msg)
     _tprintf(TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg);
 }
 
-void ListProcessThreads(DWORD dwOwnerPID)
-{
-    HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
-    THREADENTRY32 te32;
-
-    // Take a snapshot of all running threads  
-    hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (hThreadSnap == INVALID_HANDLE_VALUE)
-        return;
-
-    // Fill in the size of the structure before using it. 
-    te32.dwSize = sizeof(THREADENTRY32);
-
-    // Retrieve information about the first thread,
-    // and exit if unsuccessful
-    if (!Thread32First(hThreadSnap, &te32))
-    {
-        printError((_TCHAR*)TEXT("Thread32First")); // Show cause of failure
-        CloseHandle(hThreadSnap);          // Must clean up the snapshot object!
-        return;
-    }
-
-    // Now walk the thread list of the system,
-    // and display information about each thread
-    // associated with the specified process
-    do
-    {
-        if (te32.th32OwnerProcessID == dwOwnerPID)
-        {
-            _tprintf(TEXT("\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
-            _tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
-            _tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
-            _tprintf(TEXT("\n"));
-        }
-    } while (Thread32Next(hThreadSnap, &te32));
-
-    CloseHandle(hThreadSnap);
-    return;
-}
-
-void ListProcessModules(DWORD dwPID)
-{
-    HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
-    MODULEENTRY32 me32;
-
-    // Take a snapshot of all modules in the specified process.
-    hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
-    if (hModuleSnap == INVALID_HANDLE_VALUE)
-    {
-        printError((_TCHAR*)TEXT("CreateToolhelp32Snapshot (of modules)"));
-        return;
-    }
-
-    // Set the size of the structure before using it.
-    me32.dwSize = sizeof(MODULEENTRY32);
-
-    // Retrieve information about the first module,
-    // and exit if unsuccessful
-    if (!Module32First(hModuleSnap, &me32))
-    {
-        printError((_TCHAR*)TEXT("Module32First"));  // Show cause of failure
-        CloseHandle(hModuleSnap);           // Must clean up the snapshot object!
-        return;
-    }
-
-    // Now walk the module list of the process,
-    // and display information about each module
-    do
-    {
-        _tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
-        _tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
-        _tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
-        _tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
-        _tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
-        _tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
-        _tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
-
-    } while (Module32Next(hModuleSnap, &me32));
-
-    CloseHandle(hModuleSnap);
-    return;
-}
-
 void ListProcesses()
 {
     HANDLE hProcessSnap;
@@ -160,10 +77,6 @@ void ListProcesses()
         _tprintf(TEXT("\n  Priority base     = %d"), pe32.pcPriClassBase);
         if (dwPriorityClass)
             _tprintf(TEXT("\n  Priority class    = %d"), dwPriorityClass);
-
-        // List the modules and threads associated with this process
-        //ListProcessModules(pe32.th32ProcessID);
-        //ListProcessThreads(pe32.th32ProcessID);
 
     } while (Process32Next(hProcessSnap, &pe32));
 
